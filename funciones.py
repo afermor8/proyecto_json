2a78e23063962bee26b3e3b013188da6e66390b5
afermor8 <ara.fer.mor@gmail.com>
2022-03-01 21:16:20 +0100

    He realizado los tres primeros ejercicios.

--- funciones.py
+++ funciones.py
@@ -1,2 +1,25 @@
 #Funciones proyecto_json
 
+def listar_informacion(diccionario):
+    listatitulos= []
+    listatemporadas=[]
+    listaepisodios=[]
+
+    episodios=diccionario.get("_embedded").get("episodes")
+    for episodio in episodios:
+        listatitulos.append(episodio.get("name"))
+        listatemporadas.append(episodio.get("season"))
+        listaepisodios.append(episodio.get("number"))
+    return listatitulos,listatemporadas,listaepisodios
+
+
+def contar_informacion(diccionario):
+    return len (diccionario.get("_embedded").get("episodes"))
+
+
+def buscar_informacion(diccionario,titulo):
+    episodios=diccionario.get("_embedded").get("episodes")
+    for episodio in episodios:
+        if titulo.lower()==episodio.get("name").lower():
+            return episodio.get("summary")
+
--- principal.py
+++ principal.py
@@ -1,1 +1,31 @@
 #Programa principal
+
+import json
+from funciones import *
+
+
+#Abrir fichero json
+f=open("json_westworld.json")
+westworld = json.load(f)
+f.close()
+
+
+#Listar títulos, la temporada a la que pertenece y el número de capítulo
+titulos,temporadas,capitulos=listar_informacion(westworld)
+
+print("\n   **Westworld**\n ----------------\n")
+for titulo,temporada,capitulo in zip (titulos,temporadas,capitulos):
+    print ("Temporada:",temporada,"- Capítulo:",capitulo,"-",titulo,"\n")
+
+
+#Contar el total de episodios que tiene la serie
+print(" ---------------\n   Total de capítulos de la serie:",contar_informacion(westworld),"\n ---------------")
+
+
+#Pedir nombre de un episodio y mostrar sinopsis
+episodiopedido=input("Dime el título de un episodio: ")
+resultado=buscar_informacion(westworld,episodiopedido)
+if not resultado:
+    print ("ERROR. No existe información")
+else:
+    print ("Sinopsis:",resultado,"\n ---------------")
